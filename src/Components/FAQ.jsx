71384915c546e05191756a550fe49aca03747657
Huseyn Mirzayev <70962188+hsynmrzyv@users.noreply.github.com>
2024-07-09 15:15:10 +0400

    Update FAQ.jsx

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -5,15 +5,7 @@ const FAQ = (props) => {
   return (
     <div className="p-10">
       <ul>
-        {props.questions.map((question, index) => {
-          return (
-            <ListItem
-              onClick={props.toggle}
-              singleQuestion={question}
-              index={index}
-            />
-          );
-        })}
+       
       </ul>
     </div>
   );
