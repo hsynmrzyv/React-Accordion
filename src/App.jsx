585add11eba0568df913bab298eff434840966a2
Huseyn Mirzayev <70962188+hsynmrzyv@users.noreply.github.com>
2024-07-09 15:15:35 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,12 @@ import { useState } from "react";
 import { faqData as data } from "./data";
 
 const App = () => {
-  const [questions, setQuestions] = useState(data);
-
-  const toggleAccordion = (index) => {
-    console.log(index);
-  };
-
+ 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 font-poppins ">
       <div className="w-[500px] rounded-xl bg-white shadow-lg">
         <Header />
-        <FAQ questions={questions} toggle={toggleAccordion} />
+        <FAQ/>
       </div>
     </div>
   );
